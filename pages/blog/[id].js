90bd684a10ac7f177a2e4447f5755466e7dbd386
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-04 23:55:33 +0700

    hi

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import Head from 'next/head';
+import Header from '../components/Header';
 function News() {
-  return <div>News</div>;
+  return (
+    <div>
+      <Head>
+        <title>Stuffy - Share Stuff Application</title>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <h1 className="flex justify-center pt-10 text-2xl">Title</h1>
+      <News />
+    </div>
+  );
 }
 
 export default News;
