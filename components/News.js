1598477998aec1ec8e26770974bf3afd5e5b0679
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-04 12:42:27 +0700

    hi

--- components/News.js
+++ components/News.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardNews from './CardNews';
 
 function News() {
+  const router = useRouter();
   return (
     <div className="grid grid-cols-4 place-items-center pt-10">
       <CardNews
