d675452b399e5cefa9ea64c0ff0ffb29c4e0b0e7
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-05 14:31:27 +0700

    hi

--- pages/product.js
+++ pages/product.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 
 function product() {
   return (
-    <div className="flex items-center">
+    <div>
       <Head>
         <title>Stuffy - Share Stuff Application</title>
 
@@ -16,10 +16,11 @@ function product() {
       <h2 className="flex justify-center pt-10 text-xl">
         Stuffy App is an app for sharing stuff that have been waste
       </h2>
-
-      <button className="flex bg-white p-2 mt-10 rounded-lg font-extrabold shadow-xl">
-        Download Stuffy App v1.0.0
-      </button>
+      <div className="flex justify-center">
+        <button className="flex items-center bg-white p-2 mt-10 rounded-lg font-extrabold shadow-xl">
+          Download Stuffy App v1.0.0
+        </button>
+      </div>
     </div>
   );
 }
