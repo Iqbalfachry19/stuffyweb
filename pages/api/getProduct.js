70870e2fd79ced9bb8a59000da65ef26076bb761
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-21 23:08:40 +0700

    hi

--- pages/api/getProduct.js
+++ pages/api/getProduct.js
@@ -3,12 +3,7 @@ import { connectToDatabase } from '../../util/mongodb';
 export default async function getProduct(req, res) {
   const { db } = await connectToDatabase();
 
-  const products = await db
-    .collection('product')
-    .find({})
-    .sort({ metacritic: -1 })
-    .limit(20)
-    .toArray();
+  const products = await db.collection('product').find({}).limit(20).toArray();
 
   res.status(200).json(products);
 }
